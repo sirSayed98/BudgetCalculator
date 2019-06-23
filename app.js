cebbdfea7ffb887cd809810533156f2e3ccc22bd
mohamedsayed2011 <mohamed.sayed8899@gmail.com>
2019-06-23 16:27:26 +0200

    UI Under control ^_^

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ var o = 0;
 var totalOutcome = 0;
 var incomeList;
 var outcomeList;
-var incomeListArr = [];
-var outcomeListArr = [];
+var delButton;
+valuIn=0;
 
 /*selections */
 
@@ -59,6 +59,9 @@ function incomeEvent(val, describt) {
     btn.classList.add("item__delete--btn");
     var icon = document.createElement('i');
     icon.classList.add('ion-ios-close-outline');
+    icon.setAttribute('id', 'del-' + i);
+    icon.setAttribute('onclick', 'deleteEvent(this)')
+
     btn.appendChild(icon);
 
     itemDel.appendChild(btn);
@@ -72,8 +75,49 @@ function incomeEvent(val, describt) {
 }
 function outComeEvent(val, describt) {
     outcome[o] = new event(describt, - val);
-    o++;
     totalOutcome -= val;
+
+    var arr1 = 0;
+    arr1 = document.createElement('div');
+    outcomeList.appendChild(arr1);
+    arr1.classList.add('item', 'clearfix');
+    arr1.setAttribute('id', 'outcome-' + o);
+
+    var describtionDiv1 = document.createElement('div');
+    describtionDiv1.classList.add('item__description');
+    describtionDiv1.innerText = describtionIN;
+    arr1.appendChild(describtionDiv1);
+
+    var itemtest1 = document.createElement('div');
+    itemtest1.classList.add('right');
+    itemtest1.classList.add('clearfix');
+    arr1.appendChild(itemtest1);
+    var itemVal1 = document.createElement('div');
+    itemVal1.classList.add('item__value');
+    itemVal1.innerText = valuIn;
+
+    itemtest1.appendChild(itemVal1);
+
+    var itemDel1 = document.createElement('div');
+    itemDel1.classList.add('item__delete');
+
+    var btn1 = document.createElement('button');
+    btn1.classList.add("item__delete--btn");
+    var icon1 = document.createElement('i');
+    icon1.classList.add('ion-ios-close-outline');
+    icon1.setAttribute('id', 'del1-' + o);
+    icon1.setAttribute('onclick', 'deleteEvent(this)')
+
+    btn1.appendChild(icon1);
+
+    itemDel1.appendChild(btn1);
+    itemtest1.appendChild(itemDel1);
+
+
+
+
+
+    o++;
 
 }
 
@@ -112,6 +156,19 @@ function newEvent() {
 
 
     prepare();
+if(isNaN(valuIn))
+{
+    alert('you have to insert value')
+    prepare();
+}
+
+else if(describtionIN=='')
+{
+alert('you have to insert describtion');
+prepare();
+
+}
+else{
 
     if (operationSelector == 0) {
 
@@ -124,9 +181,37 @@ function newEvent() {
     }
     clear();
     UpdateInterface();
+}
 
 }
 
+function deleteEvent(ele) {
+
+    var id = ele.id;
+
+
+    if (id.includes('del1')) {
+        var demo = id.substr(5, 1);
+        var demo2 = Number(demo);
+
+        var element1 = document.getElementById('outcome-' + demo2);
+        totalOutcome = totalOutcome - outcome[demo2].value;
+        outcomeList.removeChild(element1);
+        UpdateInterface();
+    }
+    else {
+        var demo = id.substr(4, 1);
+        var demo2 = Number(demo);
+
+        var element = document.getElementById('income-' + demo2);
+        totalIncome = totalIncome - income[demo2].value;
+        incomeList.removeChild(element);
+        UpdateInterface();
+    }
+
+
+
+}
 
 /*main*/
 
@@ -135,6 +220,7 @@ mainButton = document.querySelector('.add__btn');
 
 
 mainButton.addEventListener("click", newEvent);
+
 
 
 
